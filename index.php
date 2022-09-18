fb211a814d8c0ea6ca63203a9fd7c296538b7c97
EsauRangel <esaurangel11@gmail.com>
2022-09-18 17:59:37 -0500

    Ejemplo simple

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Author: Dahir Muhammad Dahir
+ * Date: 12-April-2020 5:07 AM
+ * About: I will tell you later
+ */
+
+namespace fingerprint;
+
+header('Access-Controll-Allow-Origin: *');
+require("./src/html/home.html");
